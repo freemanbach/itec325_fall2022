3f543e2f0fba1fb9855777cf64ae438baf47ea2a
freeman lo <flo@radford.edu>
2022-08-22 23:57:11 -0400

    initial GIT assignment.

---
+++ week8/template.php
@@ -0,0 +1,44 @@
+<?php
+/**
+* This sample template is used for your php file upload on weekly submission.
+* There will be a theme on a weekly basis on learning this new Server centric
+* Programming language called PHP. The idea is to write your own unique code
+* to supply as a php file and to check into your local repo aka staging area
+* then to check-in that single file to this github area.
+*
+* This weekly topic will vary from week to week. There couild be examples
+* from lecture on certain native PHP functions, conditional, loopings, along
+* with parsing data to associative array. The idea of this weekly programming task
+* will be your own. I wont be making any contributions to your ideas on this weekly
+* assignment.
+*
+* Typically, there will be CLI tools, which can be run on the terminal environment
+* rather than running it via Apache as a web application. 
+*
+* To execute any PHP script as a CLI, you can run `php filename.php`
+*
+* @Author Supply your name to this single PHP file
+* @Date   Give it a date YYYY.MM.DD
+* @Email  Supply your Username@Radford.edu
+* @Desc   Explain what your goals are and what you are trying to conduct
+*/
+
+// Single Line Comment
+//
+#  Single Line Comment -- like python
+#
+/* Multi Line Comment 
+ * as in C/C++/Java
+ */
+
+// Example Code
+
+eCho "Hello Planet, Earth !\n";
+pRint "Hello Planet, Earth !\n";
+pRint("Hello Planet, Earth !\n");
+$abc = "Hello Planet, Earth !";
+pRinTF("%s \n", $abc);
+
+
+
+?>
