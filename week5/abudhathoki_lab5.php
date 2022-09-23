73484964c6ac168747e590da1e49d5652a0cc98c
Anita Budhathoki <77995552+abuda502@users.noreply.github.com>
2022-09-22 20:46:47 -0400

    Update abudhathoki_lab5.php

--- week5/abudhathoki_lab5.php
+++ week5/abudhathoki_lab5.php
@@ -1,3 +1,16 @@
 <?php
-preg_match_all('/(foo)(bar)(baz)/', 'foobarbaz', $matches, PREG_OFFSET_CAPTURE);
-print_r($matches);
+// The \\2 is an example of backreferencing. This tells pcre that
+// it must match the second set of parentheses in the regular expression
+// itself, which would be the ([\w]+) in this case. The extra backslash is
+// required because the string is in double quotes.
+$html = "<b>bold text</b><a href=howdy.html>click me</a>";
+
+preg_match_all("/(<([\w]+)[^>]*>)(.*?)(<\/\\2>)/", $html, $matches, PREG_SET_ORDER);
+
+foreach ($matches as $val) {
+    echo "matched: " . $val[0] . "\n";
+    echo "part 1: " . $val[1] . "\n";
+    echo "part 2: " . $val[2] . "\n";
+    echo "part 3: " . $val[3] . "\n";
+    echo "part 4: " . $val[4] . "\n\n";
+}
